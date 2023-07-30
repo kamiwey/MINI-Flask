52223766ee87236c640176591b77ee7007765d1f
Kamiwey <kamiwey@gmail.com>
2023-07-30 19:17:29 +0200

    done

---
+++ client/pages/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useEffect, useState } from "react";
+
+function index() {
+
+  const [message, setMessage] = useState("Loading");
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/home").then(
+      response => response.json()
+    ).then(
+      data => {
+        
+        setMessage(data.message);
+        setPeople(data.people);
+
+        console.log(data.people);
+      });
+  }, []);
+
+  return (
+    <div>
+      <div>{message}</div>
+      
+      {
+        people.map((person, index) => (
+          <div key={index}>{person}</div>
+        ))
+      }
+
+    </div>
+    
+  );
+}
+
+export default index;
